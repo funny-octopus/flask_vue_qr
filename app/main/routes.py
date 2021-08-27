3ab039de506995b7c7e252df02489b078438fd01
funny-octopus <falc_90@mail.ru>
2021-08-27 21:02:58 +0200

    Make login

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,6 +1,9 @@
 from app import app, db
 from app.main import bp
-from flask import render_template, request, flash
+from app.models import User
+from app.main.forms import LoginForm
+from flask import render_template, request, flash, redirect, url_for
+from flask_login import current_user, login_user
 
 
 @bp.route('/', methods=['GET'])
@@ -16,5 +19,20 @@ def item(ident):
 
 @bp.route('/auth/login', methods=['GET', 'POST'])
 def login():
-    return render_template('main/login.html')
+    if current_user.is_authenticated:
+        return redirect(url_for('main.index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(name=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('main.index'))
+    return render_template('main/login.html', title="Вход", form=form)
+
+
+@bp.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('main.login'))
 
--- app/models.py
+++ app/models.py
@@ -1,8 +1,15 @@
 from app import db
+from app import login
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
 
-class User(db.Model):
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), index=True, unique=True)
     password_hash = db.Column(db.String(128))
