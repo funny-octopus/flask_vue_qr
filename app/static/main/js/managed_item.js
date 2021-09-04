c9cc657c8388721462601b44c4d36081722a56f8
funny-octopus <falc_90@mail.ru>
2021-09-04 06:43:39 -0400

    Update managed_items

--- app/static/main/js/managed_item.js
+++ app/static/main/js/managed_item.js
@@ -2,7 +2,9 @@ var app = new Vue({
     el: '#app',
     delimiters: ['[[', ']]'],
     data: {
+        id:null,
         product:null, 
+        resp:null,
         select_list:['category', 'factory', 'country', 'currency', 'price_v'],
         factories:null,
         categories:null,
@@ -11,13 +13,15 @@ var app = new Vue({
     },
     methods:{
         save:function(){
-            console.log(this.product);
+            console.log(this.resp.product);
+            this.changes_flag=false;
         },
     },
     created(){
         ident = document.getElementById('ident').value;
         axios.get('/api/item/'+ident)
-            .then(response => (this.product = response.data.product));
+        // .then(response => (this.product = response.data.product;this.id = response.data.id;));
+            .then(response => (this.resp = response.data));
         axios.get('/api/items')
             .then(response => (this.items = response.data));
     },
