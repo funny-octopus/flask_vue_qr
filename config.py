ab46773bca7cfbbcbd1e35f1ffd8faa7688e8997
funny-octopus <falc_90@mail.ru>
2021-09-12 19:49:09 -0400

    Add dotenv

--- config.py
+++ config.py
@@ -1,12 +1,15 @@
 import os
+from dotenv import load_dotenv
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
+    load_dotenv()
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'bigbigsecret'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or\
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATION = False
     IMAGES_UPLOAD_FOLDER = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'app', 'static', 'images')
     IMAGES_ALLOWED_EXTENSIONS = {'jpg','jpeg','png','bmp'}
+    DOMAIN = os.environ.get('DOMAIN') or 'http://127.0.0.1:5000'
 
