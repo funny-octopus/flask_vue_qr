965c92ab2d3a4898b4833300cd89237fea1de1c6
funny-octopus <falc_90@mail.ru>
2021-09-12 23:40:32 -0400

    Update errors

--- app/errors/handlers.py
+++ app/errors/handlers.py
@@ -7,6 +7,11 @@ def not_found_error(error):
     return render_template('errors/404.html'), 404
 
 
+@bp.app_errorhandler(401)
+def internal_error(error):
+    return render_template('errors/401.html'), 401
+
+
 @bp.app_errorhandler(500)
 def internal_error(error):
     db.session.rollback()
